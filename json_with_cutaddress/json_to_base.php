cfe6f56f91c9c98274608619bd66d267b2bee5f0
Heffiros <vasax.levy@gmail.com>
2014-04-24 20:00:20 +0200

    Je met des couleurs parce que c est jolie en plus y a  un beaux logo

--- json_with_cutaddress/json_to_base.php
+++ json_with_cutaddress/json_to_base.php
@@ -1,5 +1,6 @@
 <?php
 $dbh = new PDO('mysql:host=localhost;dbname=camelfind', 'root', '');
+$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $dir = "C:\wamp\www\OpenData\json_with_cutaddress\\";
 $var = opendir($dir);
 while ($content = readdir($var))
@@ -11,21 +12,18 @@ while ($content = readdir($var))
 		foreach ($json as $key) {
 			if(isset($key['fields']['type_d_equipement']))
 				$desc = $key['fields']['type_d_equipement'];
-				if (isset($key['fields']['ap_num']))
-					$address = $key['fields']["ap_num"];
-				if (isset($key['fields']["ap_voie"]))
-					$address .= " ".$key['fields']["ap_voie"];
-				if (isset($key['fields']["ap_cp"]))
-					$address .= " ".$key['fields']["ap_cp"];
-				if (isset($key['fields']["wgs84"]))
-				{
-					$lat = $key['fields']["wgs84"][0];
-					$long = $key['fields']["wgs84"][1];
-				}
+			if (isset($key['fields']['ap_num']))
+				$address = $key['fields']["ap_num"];
+			if (isset($key['fields']["ap_voie"]))
+				$address .= " ".$key['fields']["ap_voie"];
+			if (isset($key['fields']["ap_cp"]))
+				$address .= " ".$key['fields']["ap_cp"];
+			if (isset($key['fields']["wgs84"]))
+			{
+				$lat = $key['fields']["wgs84"][0];
+				$long = $key['fields']["wgs84"][1];
 			}
-		}
-	}
-			$req = $dbh->prepare('INSERT INTO place (category_id, address, lat, lon, description) VALUES (:a, :address, :lat, :lon, :description)');
+			$req = $dbh->prepare('INSERT INTO places (category_id, address, k, a, description) VALUES (:a, :address, :lat, :lon, :description)');
 			$req->execute(array(
 				'a' => 4,
 				'address' => $address,
@@ -33,6 +31,9 @@ while ($content = readdir($var))
 				'lon' => $long,
 				'description' => $desc,
 				));
+			echo "Insert effectue";
 
 		}
-	?>
+	}
+}
+?>
