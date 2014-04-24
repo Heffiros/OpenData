d1205f1f43c0c32759a40b5feffbca22d7b4c293
maltuin <maxime.margaine@yahoo.fr>
2014-04-24 15:44:52 +0200

    Ajout du script

---
+++ json_with_cutaddress/json_to_base.php
@@ -0,0 +1,38 @@
+<?php
+$dbh = new PDO('mysql:host=localhost;dbname=camelfind', 'root', '');
+$dir = "C:\wamp\www\OpenData\json_with_cutaddress\\";
+$var = opendir($dir);
+while ($content = readdir($var))
+{
+	if ($content[0] != '.' && $content!="json_to_base.php")
+	{
+		$raw = file_get_contents($dir.$content);
+		$json = json_decode($raw, true);
+		foreach ($json as $key) {
+			if(isset($key['fields']['type_d_equipement']))
+				$desc = $key['fields']['type_d_equipement'];
+				if (isset($key['fields']['ap_num']))
+					$address = $key['fields']["ap_num"];
+				if (isset($key['fields']["ap_voie"]))
+					$address .= " ".$key['fields']["ap_voie"];
+				if (isset($key['fields']["ap_cp"]))
+					$address .= " ".$key['fields']["ap_cp"];
+				if (isset($key['fields']["wgs84"]))
+				{
+					$lat = $key['fields']["wgs84"][0];
+					$long = $key['fields']["wgs84"][1];
+				}
+			}
+		}
+	}
+			$req = $dbh->prepare('INSERT INTO place (category_id, address, lat, lon, description) VALUES (:a, :address, :lat, :lon, :description)');
+			$req->execute(array(
+				'a' => 4,
+				'address' => $address,
+				'lat' => $lat,
+				'lon' => $long,
+				'description' => $desc,
+				));
+
+		}
+	?>
